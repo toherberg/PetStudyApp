1e2686453d6420feb5b876c2c12d1ced62e288ba
anymacstore <tth4ik@gmail.com>
2022-04-01 16:20:11 +0300

    error handling basics

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -7,6 +7,7 @@ namespace API.Dtos
         [Required]
         public string UserName { get; set; }
         [Required]
+        [StringLength(8,MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -38,12 +39,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PeS WebAPI v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PeS WebAPI v1"));
+            
 
             app.UseHttpsRedirection();
 
